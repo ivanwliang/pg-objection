8dd2898aa4c53ab87ba61880075cb495c0fbf0de
Ivan Liang <ivanwenliang@gmail.com>
2019-10-31 21:37:38 -0700

    Add some test routes

--- routes/papers.js
+++ routes/papers.js
@@ -4,9 +4,21 @@ const router = express.Router();
 const Paper = require("../models/Paper");
 const Footnote = require("../models/Footnote");
 
-module.exports = router => {
-  router.get("/papers", async (req, res) => {
-    const papers = await Paper.query();
-    res.json(papers);
-  });
-};
+router.get("/papers", async (req, res) => {
+  const papers = await Paper.query();
+  res.json(papers);
+});
+
+router.get("/papers/:id", async (req, res) => {
+  const paper = await Paper.query()
+    .findById(req.params.id)
+    .eager("footnotes");
+  res.json(paper);
+});
+
+router.get("/footnotes/:id", async (req, res) => {
+  const footnote = await Footnote.query().findById(req.params.id);
+  res.json(footnote);
+});
+
+module.exports = router;
