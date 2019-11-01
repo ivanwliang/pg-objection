e998aa204aaf08efbe1e8082a78d194cc588aa00
Ivan Liang <ivanwenliang@gmail.com>
2019-10-31 20:14:46 -0700

    Create tables

---
+++ db/migrations/20191031200924_initial.js
@@ -0,0 +1,20 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("papers", table => {
+      table.increments("id").primary();
+      table.string("title");
+      table.string("author");
+      table.timestamps(true, true);
+    })
+    .createTable("footnotes", table => {
+      table.increments("id").primary();
+      table.string("note");
+      table.integer("paper_id").unsigned();
+      table.foreign("paper_id").references("papers.id");
+      table.timestamps(true, true);
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("footnotes").dropTable("papers");
+};
