91ff339ef64a4094622812dc95d6ff590c73c6d4
Ivan Liang <ivanwenliang@gmail.com>
2019-10-31 20:26:40 -0700

    Add publisher column

---
+++ db/seeds/dev/papers.js
@@ -0,0 +1,28 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("footnotes")
+    .del()
+    .then(knex("papers").del())
+    .then(() => {
+      // Inserts seed entries
+      return (
+        knex("papers")
+          // Return id after insertion -> [1]
+          .insert(
+            { title: "Fooo", author: "Bob", publisher: "Minnesota" },
+            "id"
+          )
+          .then(paper => {
+            console.log(paper);
+            return knex("footnotes")
+              .insert([
+                { note: "Lorem", paper_id: paper[0] },
+                { note: "Dolor", paper_id: paper[0] }
+              ])
+              .then(() => console.log("Seeding Complete!"))
+              .catch(error => console.log(`Error seeding data: ${error}`));
+          })
+      );
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
